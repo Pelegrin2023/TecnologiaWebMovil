13634c647725bb40b6a5bde9b4956a9f349ea707
Pelegrin2023 <pelegrin2.huichalaf2.788@gmail.com>
2023-09-15 12:55:33 -0300

    commit1

---
+++ retos/reto1/JS/reto1.js
@@ -0,0 +1,13 @@
+//Función para determinar si nhumero es par o impar
+function ParoImpar(n) {
+    if (n >=0) {
+        if (n%2 == 0 ) {
+            console.log("Número par");
+        } else {
+            console.log("Número Impar");
+        }
+    } else {
+        console.log("Numero menor que cero")
+    }   
+}
+
